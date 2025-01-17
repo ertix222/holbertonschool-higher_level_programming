d3c0e8c5765505e24cdbd2def400153eb1dc1b89
ertix222 <ertix222@gmail.com>
2025-01-17 22:05:28 +0100

    change a line placement

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for c in range(10):
     for d in range(c + 1, 10):
         if c == 8 and d == 9:
-            print(f"{c}{d}", end=", ")
-        else:
             print(f"{c}{d}")
+        else:
+            print(f"{c}{d}", end=", ")
