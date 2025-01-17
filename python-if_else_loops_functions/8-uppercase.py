25de8898b99f3c7b6690211481b8ea4cb5e3be3c
ertix222 <ertix222@gmail.com>
2025-01-17 21:59:47 +0100

    tranform into upper case

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
+        value = ord(c)
         if (ord(c) in range(97, 123)):
             c = chr(ord(c) - 32)
         print("{}".format(chr(c)), end="")
