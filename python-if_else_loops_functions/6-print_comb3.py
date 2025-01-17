db3407528594c04663d17d901de2a59dfc4fcef1
ertix222 <ertix222@gmail.com>
2025-01-17 21:17:53 +0100

    print whitout same digits differents time

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for c in range(10):
+    for d in range(c + 1, 10):
+        if c < 8 or (c == 8 and d > 9):
+            print(f"{c}{d}", end=", ")
+        else:
+            print(f"{c}{d}")
