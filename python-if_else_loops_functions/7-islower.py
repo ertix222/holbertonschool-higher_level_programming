dbf3a1a18e7552bc4b7adb7a9a3ab0c47fb74b1c
ertix222 <ertix222@gmail.com>
2025-01-17 21:38:02 +0100

    lower upper or other

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
-        print(f"{c} is lower")
-    elif ord(c) >= 65 and ord(c) <= 90:
-        print(f"{c} is upper")
+    """Check if a character is lowercase."""
+    if ord(c) >= 97 and ord(c) <= 122:  # Minuscule
+        return "{c} is lower"
+    elif ord(c) >= 65 and ord(c) <= 90:  # Majuscule
+        return "{c} is upper"
     else:
-        print(f"{c} False")
+        return False
+    
