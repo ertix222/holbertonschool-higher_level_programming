803d475ec2c692aacae61d7af6fea12bd42d0e07
ertix222 <ertix222@gmail.com>
2025-01-17 22:13:12 +0100

    t6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for c in range(0, 89):
         if (c // 10 != c % 10) and (c // 10 < c % 10):
             print("{:02d}, ".format(c), end="")
-            c += 1
-            print("{}".format(c))
+c += 1
+print("{}".format(c))
