93641282d6fb0ff977dbc9507a1050a36f8ea4b0
ertix222 <ertix222@gmail.com>
2025-01-22 03:51:35 +0100

    add tuple

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
+    tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
+    
+    a1, a2 = tuple_a[:2]
+    b1, b2 = tuple_b[:2]
+    
+    return (a1 + b1, a2 + b2)
