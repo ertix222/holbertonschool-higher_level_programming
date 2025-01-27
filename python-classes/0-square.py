a7735ec646d7d6b113e0785db5e168ee67258a2e
ertix222 <ertix222@gmail.com>
2025-01-27 10:27:04 +0100

    square

---
+++ python-classes/0-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+module 0-square
+
+Function:
+create a square class
+"""
+
+
+class Square:
+    """
+    This is my classe
+    for a square
+    """
+    pass
+
+"""
+this class is for a square
+with nothing for now
+"""
