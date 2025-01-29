6946503866348b8830044075aae69acc7c22ab01
ertix222 <ertix222@gmail.com>
2025-01-29 01:07:45 +0100

    pycodestyle fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -46,10 +46,9 @@ class Square:
         property to set the position
         """
         pass
-        if (not isinstance(value, tuple) or\
-            len(value) != 2 or\
+        if (not isinstance(value, tuple) or
+            len(value) != 2 or
             not all(isinstance(num, int) and num >= 0 for num in value)):
-            
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -83,9 +82,9 @@ class Square:
         if self.__size == 0:
             print()
             return
-        
-        for _ in range (self.__position[1]):
+
+        for _ in range(self.__position[1]):
             print()
-        
+
         for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
