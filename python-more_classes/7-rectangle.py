b3cb8c79dd775ef8d5c788db29df52e7abbf1e78
ertix222 <ertix222@gmail.com>
2025-01-30 11:05:11 +0100

    fix pycodestyle line 94 too long

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -91,7 +91,10 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         else:
-            return "\n".join(str(self.print_symbol) * self.width for _ in range(self.height))
+            return "\n".join(
+                str(self.print_symbol) * self.width
+                for _ in range(self.height)
+                )
 
     def __repr__(self):
         """
