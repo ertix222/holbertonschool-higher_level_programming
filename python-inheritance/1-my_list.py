5635e13d1db76a4647daeafad71083231f9fe764
ertix222 <ertix222@gmail.com>
2025-02-03 15:25:24 +0100

    sorted list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+module 1-my_list
+
+Fonction:
+write a class that inherits
+"""
+
+
+class MyList(list):
+    """
+    a class for Mylist
+    """
+    def print_sorted(self):
+        """
+        module for sort list
+        """
+        print(sorted(self))
