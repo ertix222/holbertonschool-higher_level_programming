91176d561320e9c619207c9a9c900f9b8d4a6ae6
ertix222 <ertix222@gmail.com>
2025-02-04 09:25:11 +0100

    empty class BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 5-base_geometry
+
+containing the Basegeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    an empty class for BaseGeommetry
+    """
+    pass
