5ca71503c2dadbc1fc5f48bdeedf1dcef20a7420
ertix222 <ertix222@gmail.com>
2025-02-05 11:15:27 +0100

    filling the docstring

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,29 +5,24 @@ module 10-square
 
 Rectangle = __import__('9-rectangle').Rectangle
 """
-
+here we import Rectangle from the file selected
 """
 
 
 class Square(Rectangle):
     """
-    
+    create a square class
     """
 
     def __init__(self, size):
-        """
-        
-        """
+
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
-        """
-        
-        """
+
         return (self.__size * self.__size)
+
     def __str__(self):
-        """
-        
-        """
+
         return (f"[Rectangle] {self.__size}/{self.__size}")
