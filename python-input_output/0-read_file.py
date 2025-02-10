a4952781613ae79ee3fdabc45680350cb2641f45
ertix222 <ertix222@gmail.com>
2025-02-10 10:27:34 +0100

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,6 @@ def read_file(filename=""):
     """
     defines a fonction that read text file
     """
-    with open("my_file_0.txt", "r", encoding="utf-8") as f:
+    with open('my_file_0.txt', "r", encoding="utf-8") as f:
         content = f.read()
     print(content)
