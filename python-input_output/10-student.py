7dfd937f75d0be9e81562fa40341928bc3752bf7
ertix222 <ertix222@gmail.com>
2025-02-11 22:39:22 +0100

    std to json with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module to defining the Student class
+"""
+
+
+class Student:
+    """
+    represents a student with a first name
+    last name, and age.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialize Student instance
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        return the dictionary representation of the student instance
+        """
+        if isinstance(attrs, list) and all(isinstance(item, str) for item in attrs):
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+        return self.__dict__
