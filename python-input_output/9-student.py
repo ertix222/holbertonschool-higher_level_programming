e01e2d85a37bd8aca61aebf3e1a01722a7621dd2
ertix222 <ertix222@gmail.com>
2025-02-11 10:05:17 +0100

    students to JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!usr/bin/python3
+"""
+Module to defining the Student class
+"""
+
+
+class Student:
+    """
+    represents a student with a first name
+    last name, and age.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialize Student instance
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        return the dictionary representation of the student instance
+        """
+        return self.__dict__
