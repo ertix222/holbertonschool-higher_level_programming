4b0e239df717a6b8d83d33e0a874d610bb6b9913
ertix222 <ertix222@gmail.com>
2025-02-14 00:25:49 +0100

    pickling custom classes

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import pickle
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+    
+    def display(self):
+        """
+        prints the object attributes
+        """
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+    
+    def serialize(self, filename):
+        """
+        serializes the object and saves it to a file
+        """
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except Exception as e:
+            print(f"Error serializing object: {e}")
+    
+    @classmethod
+    def deserialization(cls, filename):
+        """
+        """
+        try:
+            with open(filename, 'rb') as f:
+                return pickle.load(f)
+        except (FileNotFoundError, pickle.PickleError, EOFError) as e:
+            print(f"Error deserializing object: {e}")
+            return None
