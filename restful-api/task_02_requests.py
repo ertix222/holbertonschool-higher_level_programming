012fa920d20294210d345ea11b66d1812f433508
ertix222 <ertix222@gmail.com>
2025-02-19 10:17:43 +0100

    fixing pycodestyle error

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,6 +4,7 @@
 import requests
 import csv
 
+
 def fetch_and_print_posts():
     """
     """
@@ -11,11 +12,12 @@ def fetch_and_print_posts():
     response = requests.get(url)
 
     print(f"Status Code: {response.status_code}")
-    
+
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
             print(post['title'])
+
 
 def fetch_and_save_posts():
     """
@@ -26,11 +28,16 @@ def fetch_and_save_posts():
         posts = response.json()
         post_data = list()
         for post in posts:
-            post_data.append({"id": post["id"], "title": post["title"], "body": post ["body"]})
+            post_data.append({
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            })
             with open('post.csv', mode='w', newline='', encoding='utf-8') as f:
                 writer = csv.DictWriter(f, fieldnames=["id", "title", "body"])
                 writer.writeheader()
                 writer.writerows(post_data)
+
 
 fetch_and_print_posts()
 fetch_and_save_posts()
