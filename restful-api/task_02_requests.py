27e710301be36b2139ad209b3e7bbd1351ad354e
ertix222 <ertix222@gmail.com>
2025-02-21 09:52:00 +0100

    Fix t2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Module T2"""
 
 import requests
 import csv
@@ -28,17 +28,13 @@ def fetch_and_save_posts():
     response = requests.get(url)
     if response.status_code == 200:
         posts = response.json()
-        post_data = list()
-        for post in posts:
-            post_data.append({
-                "id": post["id"],
-                "title": post["title"],
-                "body": post["body"]
-            })
+
         with open('post.csv', mode='w', newline='', encoding='utf-8') as f:
             writer = csv.DictWriter(f, fieldnames=["id", "title", "body"])
             writer.writeheader()
-            writer.writerows(post_data)
+            for post in posts:
+                writer.writerow({"id": post["id"], "title": post["title"],
+                                "body": post["body"]})
 
 
 fetch_and_print_posts()
