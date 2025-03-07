9316187b4a63d289c0d2484a13779c584deed358
ertix222 <ertix222@gmail.com>
2025-03-07 15:34:27 +0100

    Dexter show genres

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- lists all genre and display the number of show linked to each one
+SELECT tv_genres.name AS genre
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
