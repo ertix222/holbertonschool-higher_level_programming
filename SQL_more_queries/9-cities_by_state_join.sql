4f6ba092618c112bf1133b0619c615a79f63617a
ertix222 <ertix222@gmail.com>
2025-03-07 10:05:02 +0100

    cities by state

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- list all cities cintained int the databaseshbtn_0d_usa
-SELECT cities.id, cities.name, (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
-FROM cities
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.id
 ORDER BY cities.id ASC;
