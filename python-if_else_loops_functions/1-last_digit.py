4108291687855e1006832f82045d83ff51584263
ertix222 <ertix222@gmail.com>
2025-05-05 14:24:44 +0200

    update digit_nbr to dgt_nbr for pycodestyle

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit_nbr = abs(number) % 10
+dgt_nbr = abs(number) % 10
 if number < 0:
-    digit_nbr = -digit_nbr
+    dgt_nbr = -dgt_nbr
 
-if digit_nbr > 5:
-    print(f"Last digit of {number} is {digit_nbr} and is greater than 5")
-elif digit_nbr < 6 and digit_nbr != 0:
-    print(f"Last digit of {number} is {digit_nbr} and is less than 6 and not 0")
+if dgt_nbr > 5:
+    print(f"Last digit of {number} is {dgt_nbr} and is greater than 5")
+elif dgt_nbr < 6 and dgt_nbr != 0:
+    print(f"Last digit of {number} is {dgt_nbr} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {digit_nbr} and is 0")
+    print(f"Last digit of {number} is {dgt_nbr} and is 0")
