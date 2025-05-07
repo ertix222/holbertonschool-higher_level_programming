bd67de57bfa1e1266d9e136699b0d1cf861174d3
ertix222 <ertix222@gmail.com>
2025-05-07 09:33:35 +0200

    0 to 99 with two digits event under 10

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for a in range(100):
     if a < 99:
-        print(f"{a:02d}, ", end="")
+        print(f"{a:02d}, ".format(chr(a)), end="")
     elif a == 99:
         print(f"{a}")
