65ea39ea3d4a72aa7d09fd980e3569b74e14ef07
ertix222 <ertix222@gmail.com>
2025-05-15 10:00:26 +0200

    return the biggest int value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
-        return 
+        return
     score_key = None
     max_value = float('-inf')
     for key, value in a_dictionary.items():
