f6f53568f8ba3bcbda862e237668d66e43cf7e8b
ertix222 <ertix222@gmail.com>
2025-05-15 10:35:44 +0200

    convert roman ton int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     rNum = {'i':1, 'v':5, 'x':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    if roman_string is None or roman_string is not str:
+        return 0
