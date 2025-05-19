4e233f1bfcbcc245d1ed9fdbefa8c59c2643537c
ertix222 <ertix222@gmail.com>
2025-05-19 10:40:53 +0200

    adding comments

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+
+"""
+create a class Square
+"""
+
+
 class Square():
+    """
+    defines a square
+    """
+
     def __init__(self, size=0):
+
+        """
+        define the size in a privates instance
+        """
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size musts be an integer")
