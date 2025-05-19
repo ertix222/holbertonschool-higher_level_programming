9e6ad34444fcd020d79f6ae6c8c1b320aa17a5fe
ertix222 <ertix222@gmail.com>
2025-05-19 11:37:34 +0200

    getter and setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square():
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("size musts be an integer")
+            raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
 
