ab4479ba9d593ad488836c940cf9480d4a1921ee
ertix222 <ertix222@gmail.com>
2025-05-19 11:34:44 +0200

    getter and setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+create a class Square
+"""
+
+
+class Square():
+    """
+    defines a square
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size musts be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size * self.__size)
