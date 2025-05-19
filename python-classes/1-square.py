0b4ec9d2a97995b4287bc82cd751e0a2d6157031
ertix222 <ertix222@gmail.com>
2025-05-19 10:38:06 +0200

    fixing blank line for pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,11 +9,11 @@ class Square():
     """
     defines a square
     """
-    
+
     def __init__(self, size):
-        
+
         """
         private instance for the size
         """
-        
+
         self.__size = size
