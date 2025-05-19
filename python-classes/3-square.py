7fcc8660e877ba16d6e2c7d548cf595f6d31e85e
ertix222 <ertix222@gmail.com>
2025-05-19 10:45:45 +0200

    adding comments

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+
+"""
+create a class Square
+"""
+
+
 class Square():
+    """
+    defines a square
+    """
+
     def __init__(self, size=0):
+
+        """
+        define the size of the square
+        in a privates instance
+        """
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size musts be an integer")
@@ -8,4 +23,8 @@ class Square():
             raise ValueError("size must be >= 0")
 
     def area(self):
+
+        """
+        return the area of the square
+        """
         return (self.__size ** self.__size)
