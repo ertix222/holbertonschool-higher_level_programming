7e8b4977580b11c668713a48d308189d7527ee4f
ertix222 <ertix222@gmail.com>
2025-05-19 10:37:00 +0200

    adding comments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+
+"""
+create a class Square
+"""
+
+
 class Square():
+    """
+    defines a square
+    """
+    
     def __init__(self, size):
+        
+        """
+        private instance for the size
+        """
+        
         self.__size = size
