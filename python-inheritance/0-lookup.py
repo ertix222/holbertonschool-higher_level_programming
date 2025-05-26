7051b325ad61082f565ae14e5006db88e3b88376
ertix222 <ertix222@gmail.com>
2025-05-26 15:12:09 +0200

    adding comments

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+
+"""
+function that return the list of avaible
+attributes and methods of an object
+"""
+
+
 def lookup(obj):
+    """
+    return the list with dir()
+    """
     return dir(obj)
