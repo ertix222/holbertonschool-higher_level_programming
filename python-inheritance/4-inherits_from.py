43ceab0fbd3ee43c59803d6e8ef20195b508fa83
ertix222 <ertix222@gmail.com>
2025-05-27 09:57:09 +0200

    pycodestyle

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@
 def inherits_from(obj, a_class):
     """Return True if obj inherits from a_class (not same type)
     """
-    return isinstance(obj, a_class) and type(obj) is not(a_class)
+    return isinstance(obj, a_class) and type(obj) is not (a_class)
