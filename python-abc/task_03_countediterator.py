0ff57e699bbfffd4d544127bdac755f21df3b439
ertix222 <ertix222@gmail.com>
2025-05-30 12:15:37 +0200

    count the number of iteration

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+"""
+
+class CountedIterator():
+    """
+    """
+    pass
+
+    def __init__(self, iterable):
+        """
+        """
+        self.iterator = iter(iterable)
+        self.count = 0
+    
+    def __next__(self):
+        """
+        """
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration
+
+    def get_count(self):
+        """
+        """
+        return self.count
