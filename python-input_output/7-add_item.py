5841281b47be5bcc8ca6a0949cf6370bb102ecff
ertix222 <ertix222@gmail.com>
2025-06-03 09:46:55 +0200

    save and load

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""module that adds items to a list and saves them in a JSON file
+"""
+
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""import json, sys and custom JSON save/load function
+"""
+
+
+filename = "add_item.json"
+
+try:
+    list = load_from_json_file(filename)
+except:
+    list = []
+
+save_to_json_file(list, filename)
