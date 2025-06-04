eebfd27817be98980b8702124db1534fcb52fe6a
ertix222 <ertix222@gmail.com>
2025-06-04 11:28:31 +0200

    student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""module that instance age and name
+and retrieves a dictionary representation
+"""
+
+
+class Student:
+    """class that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.age = age
+        self.last_name = last_name
+        self.first_name = first_name
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(
+            isinstance(item, str) for item in attrs
+        ):
+            return {
+                key: value
+                for key, value in self.__dict__.items()
+                if key in attrs
+            }
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """if an attribute exist or not set him
+        """
+        for key, value in dict.items(json):
+            setattr(self, key, value)
