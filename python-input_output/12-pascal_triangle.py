93e59a15ff4f215408b9b19d36527f1cc346f4ae
ertix222 <ertix222@gmail.com>
2025-06-04 14:13:58 +0200

    adding the skeleton of the T12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module that defines pascal_triangle function
+"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers
+    representing the Pascal's triangle of n
+    """
+    pass
