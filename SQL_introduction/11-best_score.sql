7055aa163298b200f934d5ecb82c0bfb9388c304
ertix222 <ertix222@gmail.com>
2025-06-24 10:04:36 +0200

    select all above a define score

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- select all columns 'score' and 'name' of the table, ordered them by score and filter only those are above or equal to 10
+SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
