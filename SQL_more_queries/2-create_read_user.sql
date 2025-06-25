49815c6bfabf3406b638fb2255596c6c599b7002
ertix222 <ertix222@gmail.com>
2025-06-25 09:29:32 +0200

    create user and database with user have only select privilege

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- create database and user
+CREATE DATABASE IF NOT EXISTS hbtn0d2;
+CREATE USER IF NOT EXISTS "user_0d_2"@"localhost" IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
