7b389617e3a244223fabfc83ee51cc9ede420937
ertix222 <ertix222@gmail.com>
2025-07-18 10:26:38 +0200

    fetches and list the titles for all movies by using an URL and fetches from an URL and displays the value of an element

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      const helloDiv = document.getElementById('hello');
+      helloDiv.textContent = data.hello;
+    })
+    .catch(error => {
+      console.error('Error fetching greeting:', error);
+    });
+});
